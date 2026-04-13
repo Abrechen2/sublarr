893380254febec5e145287999f57ee8a62edceda
Abrechen2 <179815050+DennisSico@users.noreply.github.com>
2026-04-13 19:09:04 +0200

    chore: merge Alembic heads for subtitle_downloads indexes

---
+++ backend/db/migrations/versions/merge_downloads_idx_head.py
@@ -0,0 +1,21 @@
+"""merge subtitle_downloads indexes into main head
+
+Revision ID: merge_downloads_idx
+Revises: g1h2i3j4k5l6, merge_hi_pref_forced_scoring
+Create Date: 2026-04-13
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "merge_downloads_idx"
+down_revision = ("g1h2i3j4k5l6", "merge_hi_pref_forced_scoring")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
